a1bd0321e877b1feb879700ffd19f75179fe7af3
arshakhakobyan7 <arshakhakobyan1912@gmail.com>
2026-07-09 16:16:10 +0400

    Added history

--- Console Calculator/Program.cs
+++ Console Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
@@ -7,11 +8,6 @@ class Program
 {
     static object Calculator(int num1, int num2, string s_str)
     {
-        if (!(num1 is int) || !(num2 is int))
-        {
-            throw new Exception("Check your numbers type.");
-        }
-
         switch (s_str)
         {
             case "+":
@@ -32,36 +28,67 @@ class Program
                 return (int)Math.Pow(num1, num2);
 
             case "%":
-                return num1 % num2;
+                return num1 * num2 / 100;
 
             default:
                 return "Invalid operator entry.";
 
         }
     }
+
+    static string[] History(int line_quantity, string file_path)
+    {
+        
+        string[] filedata = File.ReadAllLines(file_path).TakeLast(line_quantity).ToArray();
+
+        return filedata;
+    }
     static void Main(string[] args)
     {
-        int f_num, s_num;
+        string f_num, s_num;
         string sign, filepath = @"C:\Users\User\Desktop\HistoryFile.txt", content;
+        string input = "N";
 
-
-        Console.WriteLine("First Number: ");
-        f_num = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Second Number: ");
-        s_num = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Sign {+, -, *, /, **, %}: ");
-        sign = Console.ReadLine();
-
-        var result = Calculator(f_num, s_num, sign);
-        Console.WriteLine(result);
-        
-
-        if (result is int)
+        while (input == "N")
         {
-            content = $"{f_num} {sign} {s_num} = {result}";
-            File.AppendAllText(filepath, content);
+            Console.WriteLine("First Number: ");
+            f_num = Console.ReadLine();
+
+            Console.WriteLine("Second Number: ");
+            s_num = Console.ReadLine();
+
+            Console.WriteLine("Sign {+, -, *, /, **, %}: ");
+            sign = Console.ReadLine();
+
+            if (int.TryParse(f_num, out int first_num) && int.TryParse(s_num, out int second_num))
+            {
+                var result = Calculator(first_num, second_num, sign);
+                Console.WriteLine(result);
+                Console.WriteLine("\n");
+
+                if (result is int)
+                {
+                    content = $"{f_num} {sign} {s_num} = {result}\n";
+                    File.AppendAllText(filepath, content);
+                }
+
+
+                Console.WriteLine("History\n");
+                string[] lastCalculations = History(5, filepath);
+                foreach(string line in lastCalculations)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Console.WriteLine("\n");
+                Console.WriteLine(" 'N' for new calculation\n 'Q' for end");
+                input = Console.ReadLine();
+                Console.WriteLine("\n");
+            }
+            else
+                Console.WriteLine("Check your numbers validation.\n");
         }
+
+        
     }
 }
